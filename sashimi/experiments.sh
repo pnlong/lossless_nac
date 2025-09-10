64d108451ad2c1d398168c5cd11b618b46675e87
pnlong <phillipnlong@gmail.com>
2025-09-09 23:26:44 -0700

    added experiments helper

--- sashimi/experiments.sh
+++ sashimi/experiments.sh
@@ -10,6 +10,7 @@ line="----------------------------------------"
 sample_len=8192
 cuda_visible_devices="0,1"
 n_devices=2
+wandb_group="cantaloupe"
 
 ##################################################
 
@@ -23,19 +24,19 @@ echo ${line}
 echo "8 bit:"
 
 # 8 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18mono +wandb.name=musdb18mono-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18mono wandb.group=${wandb_group} +wandb.name=musdb18mono-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 # DML, 8 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18mono +wandb.name=musdb18mono-dml-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18mono wandb.group=${wandb_group} +wandb.name=musdb18mono-dml-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 echo ${line}
 echo "16 bit:"
 
 # 16 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18mono +wandb.name=musdb18mono-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18mono wandb.group=${wandb_group} +wandb.name=musdb18mono-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 # DML, 16 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18mono +wandb.name=musdb18mono-dml-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18mono wandb.group=${wandb_group} +wandb.name=musdb18mono-dml-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 echo ${line}
 
@@ -52,19 +53,19 @@ echo ${line}
 echo "8 bit:"
 
 # 8 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18stereo +wandb.name=musdb18stereo-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18stereo wandb.group=${wandb_group} +wandb.name=musdb18stereo-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 # DML, 8 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18stereo +wandb.name=musdb18stereo-dml-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18stereo wandb.group=${wandb_group} +wandb.name=musdb18stereo-dml-8bit-${sample_len}sl dataset.bits=8 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 echo ${line}
 echo "16 bit:"
 
 # 16 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18stereo +wandb.name=musdb18stereo-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-musdb18stereo wandb.group=${wandb_group} +wandb.name=musdb18stereo-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 # DML, 16 bit
-echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18stereo +wandb.name=musdb18stereo-dml-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
+echo CUDA_VISIBLE_DEVICES=${cuda_visible_devices} python -m train experiment=audio/sashimi-dml-musdb18stereo wandb.group=${wandb_group} +wandb.name=musdb18stereo-dml-16bit-${sample_len}sl dataset.bits=16 dataset.sample_len=${sample_len} trainer.devices=${n_devices}
 
 echo ${line}
 
