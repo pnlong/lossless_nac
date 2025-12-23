3ff4261eeec99e04ae1f9eca8f3921a021c892a6
pnlong <phillipnlong@gmail.com>
2025-12-23 01:10:21 -0800

    fixed bug

--- perceptual_dithering_experiment/experiment_parallelized.py
+++ perceptual_dithering_experiment/experiment_parallelized.py
@@ -350,6 +350,12 @@ def perceptual_dithering_experiment(
         output_path = f"{output_dir}/predicted.wav",
     )
 
+    # compare original and predicted waveforms
+    original_waveform = waveform.detach().cpu()
+    predicted_waveform = predicted_waveform.detach().cpu()
+    print(f"MSE: {torch.mean((original_waveform - predicted_waveform) ** 2)}")
+    print(f"MAE: {torch.mean(torch.abs(original_waveform - predicted_waveform))}")
+
     # return nothing
     return
 
